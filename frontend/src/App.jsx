6060528b0a956eafa5ad452feb47c07bad27e179
YourGitHubUsername <youremail@example.com>
2026-03-21 00:30:28 +0530

    feat: configure tailwind css and apply base styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-    </Routes>
-  )
+    <div className="w-full h-screen flex items-center justify-center bg-slate-200">
+      <h1 className="text-3xl font-bold text-gray-700">
+        Chatly Setup 🚀
+      </h1>
+    </div>
+  );
 }
 
 export default App
